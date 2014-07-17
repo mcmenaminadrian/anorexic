849e4139651171f0b1189cf2024550f37026da18
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-07-17 22:42:13 +0100

    thread spawning has stopped working but random page now pickable

--- anorexic.R
+++ anorexic.R
@@ -2,14 +2,20 @@
 #command line options
 suppressWarnings(library("optparse"))
 option_list <- list(
-  make_option(c("-t", "--thread"), type="integer", default=1,
+  make_option(c("-t", "--thread"), type="integer", default = 1,
 	help="Specify thread number"),
   make_option(c("-c", "--code"), type="character", default="codepages.csv",
 	help="Name of code page frequency csv file"),
   make_option(c("-r", "--readwrite"), type="character", default="rwpages.csv",
 	help="Name of read-write page frequency csv file"),
-  make_option(c("-s", "--spawn"), type="integer", default = 0,
-	help="Generate spawn commands up to thread [number]")
+  make_option(c("-s", "--spawn"), type="integer", default = 1,
+	help="Generate spawn commands up to thread [number]"),
+  make_option(c("-z", "--sizecode"), type="character",
+        default="codeallocs.csv",
+	help="Name of file with code alloc size data"),
+  make_option(c("-i", "--sizerw"), type="character",
+        default="memallocs.csv",
+	help="Name of file with rw memory alloc size data")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 
@@ -19,10 +25,15 @@ colnames(codefreq)<-c('frame', 'count', 'freq')
 colnames(rwfreq)<-c('frame', 'count', 'freq')
 codemax<-apply(codefreq, 2, max, na.rm=TRUE)
 rwmax<-apply(rwfreq, 2, max, na.rm=TRUE)
+codemin<-apply(codefreq, 2, min, na.rm=TRUE)
+rwmin<-apply(rwfreq, 2, min, na.rm=TRUE)
 
 #get a page
 GetACodePage <- function(freq) {
-
+  randCode <- runif(1, codemin[3], codemax[3])
+  possiblePages <- subset(codefreq, freq >= randCode)
+  pageToGet <- subset(possiblePages, freq == (apply(possiblePages, 2, min))[3]) 
+  return(pageToGet)
 }
 
 
@@ -55,8 +66,9 @@ cat("<threadml thread=\"")
 cat(opt$t)
 cat("\" xmlns=\"http://cartesianproduct.wordpress.com\">\n")
 #spawn some lines
-if (opt$s > 0 && opt$t == 1) {
-  for (i in 2 : opt$s) {
+opt$s
+if (opt$s > opt$t) {
+  for (i in opt$t : opt$s) {
     cat("<spawn thread=\'")
     cat(i)
     cat("\' />\n")
@@ -68,5 +80,5 @@ codeMaxs = c(apply(codefreq, 2, max, na.rm=TRUE))
 rwMins = c(apply(rwfreq, 2, min, na.rm=TRUE))
 rwMaxs = c(apply(rwfreq, 2, max, na.rm=TRUE))
 #pick a code page
-
+codePage<-(GetACodePage())[1]
 
