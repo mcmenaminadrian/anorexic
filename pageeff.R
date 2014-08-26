e72f6f8d38755d55e6a1810a26bcb83a04ece9eb
U-CHIME\AMcMenamin <AMcMenamin@CNU412930G.chime.net>
2014-08-26 17:34:42 +0100

    better graphing

--- pageeff.R
+++ pageeff.R
@@ -7,12 +7,16 @@ option_list <- list(
 	help="Input file name")
 )
 opt <- parse_args(OptionParser(option_list=option_list))
-
 rawopt <- read.csv(opt$file, header=TRUE)
 rawlru2 <- read.csv("lru2summ_threads1-.csv", header=TRUE);
-attach(rawdata)
-scatterplot3d(Access, Idle/Pages, Ticks,  pch=20, xlim=range(c(0, 200000000)), ylim=(c(0, 20000000)), zlim=range(c(0, 20000000000)), color="red", angle=75, type="h")
+klru <- read.csv("2ksumm.csv", header=TRUE);
+
+attach(rawopt)
+scatterplot3d(Idle/Pages, Access, Ticks,  pch=20, ylim=range(c(0, 100000000)), xlim=(c(0, 1500000)), zlim=range(c(0, 20000000000)), color="red", angle=55 )
 par(new=TRUE)
 attach(rawlru2)
-scatterplot3d(Access, Idle/Pages, Ticks,  pch=20, xlim=range(c(0, 200000000)), ylim=(c(0, 20000000)), zlim=range(c(0, 20000000000)), color="blue", angle=75, type="h")
+scatterplot3d(Idle/Pages, Access, Ticks,  pch=20, ylim=range(c(0, 100000000)), xlim=(c(0, 1500000)), zlim=range(c(0, 20000000000)), color="yellow", angle=55)
+par(new=TRUE)
+attach(klru)
+scatterplot3d(Idle/Pages, Access, Ticks,  pch=20, ylim=range(c(0, 100000000)), xlim=(c(0, 1500000)), zlim=range(c(0, 20000000000)), color="blue", angle=55)
 
